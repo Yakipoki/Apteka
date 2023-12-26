abb87b9945fd2264ddc77ca3944aa0c1eb2412bd
f@gmail.com <ludaosina79@gmail.com>
2023-12-26 13:24:31 +0300

    финальная версия

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Database settings 
+
+DATABASE_PATH = '../apteka/src/server/database'
+DATABASE_NAME = 'pharmacy.db'
+
+# Server settings
+
+HOST = '127.0.0.1'
+PORT = 8000
+URL = f'http://{HOST}:{PORT}'
